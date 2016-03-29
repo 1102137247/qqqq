9ca53c390e392bab49917f147ce8bbe2912700d9
陳 璿仁 <fastfurious099@yahoo.com.tw>
2016-03-29 22:31:09 +0800

    123

---
+++ qqqq/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(qqqq.Startup))]
+namespace qqqq
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
